1cfffc6a185b774602dccc41deabf0ce8ec9124d
Faysal <82079632+FJay81@users.noreply.github.com>
2023-04-16 15:25:02 +0100

    Update main.c

--- Conditional loops/main.c
+++ Conditional loops/main.c
@@ -1,6 +1,6 @@
 #include "pico/stdlib.h"
 
-void extern funcs; //this just defines the funcs funtion in the c file, it is not 100% needed from my testing as you can see however it can make your code more understandable
+void extern funcs(); //this just defines the funcs funtion in the c file, it is not 100% needed from my testing as you can see however it can make your code more understandable
 
 int main()
 {
